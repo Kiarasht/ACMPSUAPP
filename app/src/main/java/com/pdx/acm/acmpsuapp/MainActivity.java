31f0e50f6c05b3c2056e6c07bd027717a38086a0
restart32 <kiarashbikar@gmail.com>
2015-09-04 19:54:52 -0700

    Actual drawer pages were implemented. Clicking on each one of them now gives a unique page. Only major part left is to actually design each of these. Everything else should be at a good point.

--- app/src/main/java/com/pdx/acm/acmpsuapp/MainActivity.java
+++ app/src/main/java/com/pdx/acm/acmpsuapp/MainActivity.java
@@ -1,16 +1,12 @@
 package com.pdx.acm.acmpsuapp;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity
@@ -43,10 +39,33 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
+        Fragment objFragment = null;
+
+        switch (position) {
+            case 0:
+                objFragment = new home_Fragment();
+                break;
+            case 1:
+                objFragment = new calender_Fragment();
+                break;
+            case 2:
+                objFragment = new signup_Fragment();
+                break;
+            case 3:
+                objFragment = new feedback_Fragment();
+                break;
+            case 4:
+                objFragment = new settings_Fragment();
+                break;
+            case 5:
+                objFragment = new about_Fragment();
+                break;
+        }
+
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .replace(R.id.container, objFragment)
                 .commit();
     }
 
@@ -86,44 +105,4 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_main, container, false);
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
-
 }
---
+++ app/src/main/java/com/pdx/acm/acmpsuapp/signup_Fragment.java
@@ -0,0 +1,20 @@
+package com.pdx.acm.acmpsuapp;
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class signup_Fragment extends Fragment {
+    View rootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.signup_layout, container, false);
+        return rootView;
+    }
+}
