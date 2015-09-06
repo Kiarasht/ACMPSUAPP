33689fe16880eec50b62bfb865f4799aa15630c5
restart32 <kiarashbikar@gmail.com>
2015-09-05 17:21:40 -0700

    Implemented the new ads created by google. This can be turned off by commenting out 3 pieces of codes in AndroidManifest.xml, about_Fragment.java, and about_layout.xml.

--- app/src/main/java/com/pdx/acm/acmpsuapp/about_Fragment.java
+++ app/src/main/java/com/pdx/acm/acmpsuapp/about_Fragment.java
@@ -1,12 +1,14 @@
 package com.pdx.acm.acmpsuapp;
 
-
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class about_Fragment extends Fragment {
     View rootView;
@@ -15,6 +17,9 @@ public class about_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.about_layout, container, false);
+        AdView mAdView = (AdView) rootView.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
         return rootView;
     }
 }
