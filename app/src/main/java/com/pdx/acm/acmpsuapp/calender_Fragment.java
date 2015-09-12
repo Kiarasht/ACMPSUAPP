d1ee1fcf9a245e634b01437e8bc2b5c58129ece6
restart32 <kiarashbikar@gmail.com>
2015-09-11 19:15:28 -0700

    Program can now hold and load html into an asset folder instead of using strings.

--- app/src/main/java/com/pdx/acm/acmpsuapp/calender_Fragment.java
+++ app/src/main/java/com/pdx/acm/acmpsuapp/calender_Fragment.java
@@ -2,13 +2,11 @@ package com.pdx.acm.acmpsuapp;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 /**
  * calender_Fragment is the class managing the Calender drawer responsible of holding dates for
@@ -27,8 +25,5 @@ public class calender_Fragment extends Fragment {
 
         return rootView;
     }
-
 }
-
-//<iframe width='100%' height='600' frameborder='0' scrolling='no' src='http://docs.google.com/viewer?url=http://thl.s3.amazonaws.com/books/featured/collectionhorsetrainingtips.pdf&embedded=true' ></iframe>
 
