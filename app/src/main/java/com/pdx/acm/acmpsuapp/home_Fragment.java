f6e58654cd6b41bb61b6d635417c32948d6904e4
restart32 <kiarashbikar@gmail.com>
2015-09-21 23:37:51 -0700

    Changes to Home:
    - Added the description in home
    - The description is now scrollable and more text can be added.

--- app/src/main/java/com/pdx/acm/acmpsuapp/home_Fragment.java
+++ app/src/main/java/com/pdx/acm/acmpsuapp/home_Fragment.java
@@ -1,12 +1,14 @@
 package com.pdx.acm.acmpsuapp;
 
 
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 /**
  * home_Fragment is the main page of the applications and it holds the logo of the organization at
@@ -20,6 +22,9 @@ public class home_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.home_layout, container, false);
+        WebView view = (WebView) rootView.findViewById(R.id.textView3);
+        view.loadUrl("file:///android_asset/about.html");
+        view.setBackgroundColor(Color.TRANSPARENT);
         return rootView;
     }
 }
