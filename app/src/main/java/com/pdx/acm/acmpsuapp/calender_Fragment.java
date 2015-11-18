0fb612ca547566668d06a027dd9e80cd62ac8838
Restart <torkian@pdx.edu>
2015-11-17 17:58:39 -0800

    Updated calendar

--- app/src/main/java/com/pdx/acm/acmpsuapp/calender_Fragment.java
+++ app/src/main/java/com/pdx/acm/acmpsuapp/calender_Fragment.java
@@ -2,29 +2,98 @@ package com.pdx.acm.acmpsuapp;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+
+import com.github.tibolte.agendacalendarview.AgendaCalendarView;
+import com.github.tibolte.agendacalendarview.CalendarPickerController;
+import com.github.tibolte.agendacalendarview.models.CalendarEvent;
+import com.github.tibolte.agendacalendarview.models.DayItem;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+import butterknife.ButterKnife;
 
 /**
  * calender_Fragment is the class managing the Calender drawer responsible of holding dates for
  * events users can look up.
  */
-public class calender_Fragment extends Fragment {
+public class calender_Fragment extends Fragment implements CalendarPickerController {
+
+    private String TAG = "com.pdx.acm.acmpsuapp";
+    Toolbar mToolbar;
+    AgendaCalendarView mAgendaCalendarView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.calender_layout, container, false);
-        WebView webView = (WebView) rootView.findViewById(R.id.webView2);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.google.com/calendar/embed?showPrint=0&showCalendars=0&" +
-                "showTz=0&wkst=2&bgcolor=%23ffffff&src=pdx.edu_mui9uk67ft1p9chrmnctcu0eug%4" +
-                "0group.calendar.google.com&ctz=America/Los_Angeles");
+        ButterKnife.bind(rootView);
+
+        mAgendaCalendarView = (AgendaCalendarView) rootView.findViewById(R.id.agenda_calendar_view);
+        mToolbar = (Toolbar) rootView.findViewById(R.id.activity_toolbar);
+
+       //((AppCompatActivity)getActivity()).setSupportActionBar(mToolbar);
+
+        Calendar minDate = Calendar.getInstance();
+        Calendar maxDate = Calendar.getInstance();
+
+        minDate.add(Calendar.MONTH, -2);
+        minDate.set(Calendar.DAY_OF_MONTH, 1);
+        maxDate.add(Calendar.YEAR, 1);
+
+        List<CalendarEvent> eventList = new ArrayList<>();
+        mockList(eventList);
+
+        mAgendaCalendarView.init(eventList, minDate, maxDate, Locale.getDefault(), this);
+
         return rootView;
+    }
+
+    @Override
+    public void onDaySelected(DayItem dayItem) {
+        Log.d(TAG, String.format("Selected day: %s", dayItem));
+    }
+
+    @Override
+    public void onEventSelected(CalendarEvent event) {
+        Log.d(TAG, String.format("Selected event: %s", event));
+    }
+
+    private void mockList(List<CalendarEvent> eventList) {
+        Calendar startTime1 = Calendar.getInstance();
+        Calendar endTime1 = Calendar.getInstance();
+        endTime1.add(Calendar.MONTH, 1);
+        CalendarEvent event1 = new CalendarEvent("Thibault travels in Iceland", "A wonderful journey!", "Iceland",
+                ContextCompat.getColor(getContext(), R.color.orange_dark), startTime1, endTime1, true);
+        eventList.add(event1);
+
+        Calendar startTime2 = Calendar.getInstance();
+        startTime2.add(Calendar.DAY_OF_YEAR, 1);
+        Calendar endTime2 = Calendar.getInstance();
+        endTime2.add(Calendar.DAY_OF_YEAR, 3);
+        CalendarEvent event2 = new CalendarEvent("Visit to Dalvík", "A beautiful small town", "Dalvík",
+                ContextCompat.getColor(getContext(), R.color.yellow), startTime2, endTime2, true);
+        eventList.add(event2);
+
+        Calendar startTime3 = Calendar.getInstance();
+        Calendar endTime3 = Calendar.getInstance();
+        startTime3.set(Calendar.HOUR_OF_DAY, 14);
+        startTime3.set(Calendar.MINUTE, 0);
+        endTime3.set(Calendar.HOUR_OF_DAY, 15);
+        endTime3.set(Calendar.MINUTE, 0);
+        CalendarEvent event3 = new CalendarEvent("Visit of Harpa", "", "Dalvík",
+                ContextCompat.getColor(getContext(), R.color.blue_dark), startTime3, endTime3, false);
+        eventList.add(event3);
     }
 }
 
