ee82d97059739d40885114f05dc44326c0f31ad3
restart32 <kiarashbikar@gmail.com>
2015-11-18 17:35:34 -0800

    Fixed the parsing issue.

--- app/src/main/java/com/pdx/acm/acmpsuapp/calender_Fragment.java
+++ app/src/main/java/com/pdx/acm/acmpsuapp/calender_Fragment.java
@@ -1,18 +1,16 @@
 package com.pdx.acm.acmpsuapp;
 
 import android.app.Dialog;
-import android.content.Intent;
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.github.tibolte.agendacalendarview.AgendaCalendarView;
@@ -21,10 +19,8 @@ import com.github.tibolte.agendacalendarview.models.CalendarEvent;
 import com.github.tibolte.agendacalendarview.models.DayItem;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -36,11 +32,12 @@ import butterknife.ButterKnife;
  */
 public class calender_Fragment extends Fragment implements CalendarPickerController {
 
-    String TAG = "com.pdx.acm.acmpsuapp";
-    Toolbar mToolbar;
-    AgendaCalendarView mAgendaCalendarView;
-    Date dateTime;
-    Date dateTime2;
+    private String TAG = "com.pdx.acm.acmpsuapp";
+    private Context context;
+    private String dateTime;
+    private String dateTime2;
+    protected Toolbar mToolbar;
+    protected AgendaCalendarView mAgendaCalendarView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -48,6 +45,7 @@ public class calender_Fragment extends Fragment implements CalendarPickerControl
 
         View rootView = inflater.inflate(R.layout.calender_layout, container, false);
         ButterKnife.bind(rootView);
+        context = getContext();
 
         mAgendaCalendarView = (AgendaCalendarView) rootView.findViewById(R.id.agenda_calendar_view);
         mToolbar = (Toolbar) rootView.findViewById(R.id.activity_toolbar);
@@ -74,24 +72,32 @@ public class calender_Fragment extends Fragment implements CalendarPickerControl
 
     @Override
     public void onEventSelected(CalendarEvent event) {
+        Log.d(TAG, "Selected day: " + event);
         try {
-            dateTime = timeformat(event.getStartTime().get(Calendar.HOUR_OF_DAY) +
-                    ":" + event.getStartTime().get(Calendar.MINUTE));
-            dateTime2 = timeformat(event.getEndTime().get(Calendar.HOUR_OF_DAY) +
-                    ":" + event.getEndTime().get(Calendar.MINUTE));
+            dateTime = timeformat(event.getStartTime().get(Calendar.HOUR_OF_DAY),
+                    event.getStartTime().get(Calendar.MINUTE));
+            dateTime2 = timeformat(event.getEndTime().get(Calendar.HOUR_OF_DAY),
+                    event.getEndTime().get(Calendar.MINUTE));
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
-        final Dialog dialog = new Dialog(getContext(), R.style.popout_title);
+        final Dialog dialog = new Dialog(context, R.style.popout_title);
         dialog.setContentView(R.layout.layout_popup);
         dialog.setTitle(event.getTitle());
         dialog.setCancelable(true);
 
         TextView text = (TextView) dialog.findViewById(R.id.TextView01);
-        String eventtext = "Location: " + event.getLocation() + "\n" +
-                "Time: " + dateTime + " " + dateTime2;
-        text.setText(eventtext);
+        StringBuilder eventdefine = new StringBuilder();
+        eventdefine.append("Location: ")
+                .append(event.getLocation())
+                .append("\nTime: ")
+                .append(dateTime)
+                .append(" to ")
+                .append(dateTime2)
+                .append("\n")
+                .append(event.getDescription());
+        text.setText(eventdefine);
 
         Button button = (Button) dialog.findViewById(R.id.Button01);
         button.setOnClickListener(new View.OnClickListener() {
@@ -110,15 +116,22 @@ public class calender_Fragment extends Fragment implements CalendarPickerControl
         startTime.set(Calendar.MINUTE, 30);
         endTime.set(Calendar.HOUR_OF_DAY, 20);
         endTime.set(Calendar.MINUTE, 0);
-        CalendarEvent event = new CalendarEvent("Github workshop by placeholder", "", "FAB-86-01",
-                ContextCompat.getColor(getContext(), R.color.blue_dark), startTime, endTime, false);
+        CalendarEvent event = new CalendarEvent("Github workshop by placeholder",
+                "Git and other version control systems are an essential tool for anybody within" +
+                        " Computer Science. As a student, you can use version control to backup" +
+                        " and track your changes to homework. If you realize you have gone down" +
+                        " the wrong path, git makes it easy to turn back time and return to a" +
+                        " previous version of it. Furthermore, in the workforce version control" +
+                        " will be the core method used to collaborate with your team on a daily" +
+                        " basis. It cannot hurt to have this skill on your resume now!" +
+                        " This workshop introduces you to the world of version control using git" +
+                        " and helps you start exploring.", "FAB-86-01",
+                ContextCompat.getColor(context, R.color.blue_dark), startTime, endTime, false);
         eventList.add(event);
     }
 
-    private static Date timeformat(String time) throws ParseException {
-        SimpleDateFormat simpledateformat = new SimpleDateFormat("HH:mm", Locale.US);
-        simpledateformat.setLenient(false);
-        return simpledateformat.parse(time);
+    private static String timeformat(int hours, int minutes) throws ParseException {
+        return String.format("%02d:%02d", hours, minutes);
     }
 }
 
